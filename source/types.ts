3e064998c7068196f0267caf401e3984317509d4
4uruanna <adrien@villalonga.software>
2026-06-22 00:04:02 +0200

    fixture: Change Factory to be less strict for compatibility

--- source/types.ts
+++ source/types.ts
@@ -1,7 +1,8 @@
 // Copyright 2026 Villalonga Software. All rights reserved. Apache-2.0 license.
 
 export type Factory<T> = new (
-  ...args: ConstructorParameters<new (...args: unknown[]) => T>
+  // deno-lint-ignore no-explicit-any
+  ...args: ConstructorParameters<new (...args: any[]) => T>
 ) => T;
 
 export type UnknownFactory = Factory<unknown>;
